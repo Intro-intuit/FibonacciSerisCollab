c1b4c0fec1febf5102504b4cc1ed1b0ae857cc1d
Intro-intuit <srikanthrg77@gmail.com>
2025-03-14 13:53:50 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,16 +1,36 @@
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
+        int[] numbers = {34, 87, 10, 56, 9, 2};
 
-        int[] fibonacci = new int[10];
-        fibonacci[0] = 0;
-        fibonacci[1] = 1;
+        bubbleSort(numbers);
 
-        for (int i=2; i<10; ++i)
-            fibonacci[i] = fibonacci[i-1] + fibonacci[i-2];
-
-        System.out.println("Fibonacci sequence:");
-        for (int num : fibonacci)
+        System.out.println("Sorted array: ");
+        for (int num : numbers)
             System.out.print(num + " ");
+    }
+
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        boolean swapped;
+
+        for (int i=0; i < n-1; i++) {
+
+            swapped = false;
+
+            for (int j=0; j < n-i-1; j++) {
+                if (arr[j] > arr[j+1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                    swapped = true;
+                }
+            }
+
+            if (!swapped)
+                break;
+        }
     }
 }
